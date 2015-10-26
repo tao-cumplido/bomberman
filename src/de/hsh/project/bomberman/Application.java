49aa7444992fbe5aae303f8218584cf7bd90acb8
xueer2zm <zheqi.lyu@stud.hs-hannover.de>
2015-10-26 12:36:22 +0100

    no message

--- src/de/hsh/project/bomberman/Application.java
+++ src/de/hsh/project/bomberman/Application.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 
 public class Application {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(Game::new);
+        Game game = new Game();
     }
 }
